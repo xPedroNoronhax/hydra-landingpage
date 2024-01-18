b33a97a8dec64aa72a1d66c1654d98e3c597eb70
Pedro Noronha <pedro_noronha@outlook.com>
2024-01-18 15:18:38 -0300

    Ajustandos about e vrpic

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,13 +1,14 @@
 import { AnnounceButton } from '../../components/Botoes'
 import { LightTitle, StrongTitle } from '../../components/Titulo/styles'
 import styles from './About.module.css'
+import image from '../../assets/images/aboutpic.png'
 const About = () => {
   return(
     <div>
       <div className={styles.about}>
 
         <div className={styles.about_pic} >
-          <img src="https://s3-alpha-sig.figma.com/img/1cc9/d296/7acef42bde421ac41ea8e5c2550b903d?Expires=1704067200&Signature=kfeMb0BMy9lCvSt-msChrU9NjqaFTq5vPFGujzmVx19FHV1DQaMJYiObNoB4hi0mGevJ-7slVLYhPT3jxVRUKrRL-D5Pt80KPZn3RmvZ-mXEjwtm5V6nKtNmHDO54SOvamJKHi33Hdgy66XronPxZbW3vBXdDhM9u9UbJ1M5iE7Ui~Y4Q28-EQyAQt5aH7WaYZ4XjZvJdLfn8UO4gTU06f46fktF3dJavVaaKzEbz1N1yS7au0Ntz3GY2PQE1mSDw6iK8sqL6bZv4MoGXZ0OJ5oFGTJO8IMfAmNE-NbV41l1rOWl6LtwS7hv1PE7b~ZgPMfB42QawtcYKIViuFlGYQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+          <img src={image} alt="" />
         </div>
 
         <div className={styles.about_txt}>
--- src/containers/VRpic/index.tsx
+++ src/containers/VRpic/index.tsx
@@ -1,9 +1,10 @@
 import styles from './VRpic.module.css'
+import image from '../../assets/images/pexels-michelangelo-buonarroti-8728382 1.png'
 
 const VRpic = () => {
   return (
     <div className={styles.hero_pic} >
-      <img className={styles.hero_pic_img} src="https://s3-alpha-sig.figma.com/img/75b1/5b2f/2d70750174aa1b1c7e5c1f0c3e829798?Expires=1704067200&Signature=WiYrNNxONCmsNNukCIGHl~ExKKDMs6dvTp~wQMTCxVZMvNZb35odLa8F5WGz47uPsxMpxUEVUIyl5lXI3pZwLuYeH5f59g~KsZUL~h9AP0k24TU6zu15ar3~G5bRqnrLAqGGe6hMgoiP18hBFAxaGcbowWlVzFi0CBwxomDJKo9o~Nxy2W2geZXIoL152Q~XbeG2nh1CE7WmjhNuEUrb242GeNfbzLnjTFHrfpRHomWcUuRM3xEZ4gmqSe5NUwlLN7IBkyg7zYCEiUUJShT~so2PCVfhYFdkcKvtMWr~U27NADSCKDbZ3~UYHNp2oVDIZRFuv41TD9eHZK6-D5IggA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+      <img className={styles.hero_pic_img} src={image} alt="" />
     </div>
   )
 }
